3626830f2cffed6b9ea9a0bbb310ed7b4d0fa628
Mike Felida <mike.felida@gmail.com>
2025-03-06 16:47:32 +0100

    Should have included this long ago =\

---
+++ include/FdF_settings.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FdF_settings.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mifelida <mifelida@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/05 16:27:33 by mifelida          #+#    #+#             */
+/*   Updated: 2025/03/05 16:30:09 by mifelida         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_SETTING_H
+# define FDF_SETTING_H
+
+# define WINDOW_WIDTH 1600
+# define WINDOW_HEIGHT 900
+
+# define WINDOW_TITLE "FdF"
+#endif // !FDF_SETTING_H

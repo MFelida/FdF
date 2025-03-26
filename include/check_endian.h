3a1030d94469fa05b70dfaf0a9cb00f569ef4a98
Mike Felida <mifelida@student.codam.nl>
2025-03-26 15:36:17 +0100

    Norm

--- include/check_endian.h
+++ include/check_endian.h
@@ -6,7 +6,7 @@
 /*   By: mifelida <mifelida@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 20:00:21 by mifelida          #+#    #+#             */
-/*   Updated: 2025/03/04 14:41:24 by mifelida         ###   ########.fr       */
+/*   Updated: 2025/03/26 15:29:22 by mifelida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@
 	&& defined (__ORDER_BIG_ENDIAN__)
 #  if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 #   define IS_LITTLE_ENDIAN 1
+#   define IS_BIG_ENDIAN 0
 #  elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
 #   define IS_LITTLE_ENDIAN 0
+#   define IS_BIG_ENDIAN 1
 #  else
 #   error "Unknown byte order"
 #  endif // __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-# define IS_BIG_ENDIAN !(IS_LITTLE_ENDIAN)
 # else
 #  error "Unknown byte order"
 # endif // defined (__BYTE_ORDER__) && defined (__ORDER_LITTLE_ENDIAN__)
-	   // && defined (__ORDER_BIG_ENDIAN__)
+	// && defined (__ORDER_BIG_ENDIAN__)
 
 #endif // !CHECK_ENDIAN_H
-
